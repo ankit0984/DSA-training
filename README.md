3a7bd9515c431fd4c936db0223eaa86baade7047
Ankit kumar <70026576+ankit0984@users.noreply.github.com>
2024-07-08 06:14:13 +0000

    bitwise# Please enter the commit message for your changes. Lines starting

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DSA-training
+# bitwise opreator
--- dem.py
+++ dem.py
@@ -1,3 +1,25 @@
-hexadecimal_number = '9db5'
-binary_number = bin(int(hexadecimal_number, 16))[2:]
-print("Binary value:", binary_number)
+binary_number = '1001110110110101'
+hexadecimal_number = ""
+
+# Group the binary number into groups of 4 digits
+while len(binary_number) % 4 != 0:
+    binary_number = '0' + binary_number
+
+# Convert each group of 4 digits to a hexadecimal digit
+for i in range(0, len(binary_number), 4):
+    group = binary_number[i:i+4]
+    decimal_value = 0
+    power = 3
+
+    # Convert the binary group to decimal
+    for digit in group:
+        decimal_value += int(digit) * (2 ** power)
+        power -= 1
+
+    # Convert the decimal value to hexadecimal
+    if decimal_value < 10:
+        hexadecimal_number += str(decimal_value)
+    else:
+        hexadecimal_number += chr(decimal_value + 55)
+
+print("Hexadecimal value:", hexadecimal_number)
